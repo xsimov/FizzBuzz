32dc93b0641ec5e298f996da0ad5b71e74ee4e14
Xavier Simó <xsimov@gmail.com>
2015-08-22 14:09:28 +0200

    Fixed tests descriptions

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,38 +10,40 @@ rules = {
 }
 describe "FizzBuzz game" do
 
-  let(:game) { FizzBuzz.new(rules) }
+  describe "Rafa de Castro's rules in IronHack" do
+    let(:game) { FizzBuzz.new(rules) }
 
-  it "must return a string" do
-    expect(game.apply_rules_for(2)).to be_a(String)
-  end
+    it "returns a string" do
+      expect(game.apply_rules_for(2)).to be_a(String)
+    end
 
-  it "must return itself as a string when any rules apply to it" do
-    expect(game.apply_rules_for(2)).to eq("2")
-  end
+    it "returns the number as a string when any rules apply to it" do
+      expect(game.apply_rules_for(2)).to eq("2")
+    end
 
-  it "must return Fizz when 3" do
-    expect(game.apply_rules_for(3)).to eq("Fizz")
-  end
+    it "returns Fizz when 3" do
+      expect(game.apply_rules_for(3)).to eq("Fizz")
+    end
 
-  it "must return Buzz when 5" do
-    expect(game.apply_rules_for(5)).to eq("Buzz")
-  end
+    it "returns Buzz when 5" do
+      expect(game.apply_rules_for(5)).to eq("Buzz")
+    end
 
-  it "must return Zazz when 7" do
-    expect(game.apply_rules_for(7)).to eq("Zazz")
-  end
+    it "returns Zazz when 7" do
+      expect(game.apply_rules_for(7)).to eq("Zazz")
+    end
 
-  it "must return FizzZazzBlu when 21" do
-    expect(game.apply_rules_for(21)).to eq("FizzZazzBlu")
-  end
+    it "returns FizzZazzBlu when 21" do
+      expect(game.apply_rules_for(21)).to eq("FizzZazzBlu")
+    end
 
-  it "must return Blu when number contains 1" do
-    expect(game.apply_rules_for(17)).to eq("Blu")
-  end
+    it "returns Blu when number contains 1" do
+      expect(game.apply_rules_for(17)).to eq("Blu")
+    end
 
-  it "must return GOD!! when number is 47" do
-    expect(game.apply_rules_for(47)).to eq("GOD!!")
+    it "returns GOD!! when number is 47" do
+      expect(game.apply_rules_for(47)).to eq("GOD!!")
+    end
   end
 
   it "has the basic rules if no rules are passed on instantiation" do
